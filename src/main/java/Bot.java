95bc3b408fb546110184a0fbbc91808634970742
Elodie Berthaud <elodie.berthaud@epitech.eu>
2019-11-30 12:02:32 +0100

    [ BOT ] - Resolved spacing between method and first bracket

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -12,22 +12,22 @@ public class Bot {
 
     Bot(){}
 
-    public void setUserName(String userName){
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public String getUserName(){
+    public String getUserName() {
         return this.userName;
     }
 
-    public String getResponse(){
+    public String getResponse() {
         if (this.response != null){
             return this.response;
         }
         return "";
     }
 
-    public boolean receiveMessage(String message){
+    public boolean receiveMessage(String message) {
 
         if (message != null){
             return (this.parseMessage(message));
@@ -35,7 +35,7 @@ public class Bot {
         return false;
     }
 
-    private void formatResponse(){
+    private void formatResponse() {
         Date today = new Date();
         SimpleDateFormat formater;
         formater = new SimpleDateFormat("EEEE d MMMM 'et il est' hh'h'mm");
@@ -43,8 +43,7 @@ public class Bot {
         this.response =  "[" + this.botName + "] " + "Salut " + this.userName + " ! Nous sommes " + formater.format(today) + ".";
     }
 
-    private boolean parseMessage(String message){
-
+    private boolean parseMessage(String message) {
         if (message.equals("@hello")){
             this.botName = "hello";
             this.formatResponse();
